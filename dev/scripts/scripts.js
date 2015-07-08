e19c511265f66b3e9787b89d7824dc8028d0fe06
beefchimi <curtdulmage@gmail.com>
2015-07-08 11:05:10 -0400

    updated hours

--- dev/scripts/scripts.js
+++ dev/scripts/scripts.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 		var ajax = new XMLHttpRequest();
 
-		ajax.open('GET', 'assets/img/svg.svg?v=1', true);
+		ajax.open('GET', 'assets/img/svg.svg?v=3', true);
 		ajax.send();
 		ajax.onload = function(e) {
 
